54bb46e0a7575555116717685cfe1ffbb673a23e
GakuA <akazawagaku@gmail.com>
2017-11-23 14:42:58 +0900

    first

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css" type="text/css">
+    <title>Smart Tube</title>
+</head>
+<body>
+    <div class="all">
+        <div>
+            <form>
+                <input class="url" type="text">
+                <input type="submit" value="投稿">
+            </form>
+        </div>
+    </div>
+</body>
+</html>
