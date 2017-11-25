b444e0de0e125bd201189bcc9de8a66945604ae6
GakuA <akazawagaku@gmail.com>
2017-11-25 09:53:44 +0900

    up

--- index.php
+++ index.php
@@ -9,26 +9,72 @@
 </head>
 <body>
     <div class="all">
-        <div>
-            <form>
-                <input class="url" type="text">
-                <input class="tokoBtm" type="submit" value="投稿">
-            </form>
+        <div class="main">
+            <div>
+                <form>
+                    <input class="url" type="text" placeholder="YouTube、またはニコニコ動画のURLを投稿してください">
+                    <input class="tokoBtm" type="submit" value="投稿">
+                </form>
+            </div>
+
+            <div class="tab">
+                <div>スポーツ</div><!--
+                --><div>テクノロジー</div><!--
+                --><div>ニュース</div><!--
+                --><div>ゲーム</div><!--
+                --><div>動物</div><!--
+                --><div>商品紹介</div><!--
+                --><div>部屋紹介</div><!--
+                --><div>テレビ番組</div><!--
+                --><div>YouTuber</div><!--
+                --><div>凄技</div><!--
+                --><div>音楽</div><!--
+                --><div>筋肉</div>
+            </div>
         </div>
 
-        <div class="tab">
-            <div>スポーツ</div><!--
-            --><div>テクノロジー</div><!--
-            --><div>ニュース</div><!--
-            --><div>ゲーム</div><!--
-            --><div>動物</div><!--
-            --><div>商品紹介</div><!--
-            --><div>部屋紹介</div><!--
-            --><div>テレビ番組</div><!--
-            --><div>YouTuber</div><!--
-            --><div>凄技</div><!--
-            --><div>音楽</div><!--
-            --><div>筋肉</div>
+        <div class="setting">
+            <div>
+                <h4>興味のあるジャンルを選択して下さい</h4>
+            </div>
+            <form class="select">
+                <label>
+                    <input type="checkbox" id="sports" name="select[]">スポーツ
+                </label>
+                <label>
+                    <input type="checkbox" id="technology" name="select[]">テクノロジー
+                </label>
+                <label>
+                    <input type="checkbox" id="news" name="select[]">ニュース
+                </label>
+                <label>
+                    <input type="checkbox" id="game" name="select[]">ゲーム
+                </label>
+                <label>
+                    <input type="checkbox" id="game" name="select[]">動物
+                </label>
+                <label>
+                    <input type="checkbox" id="game" name="select[]">商品紹介
+                </label>
+                <label>
+                    <input type="checkbox" id="game" name="select[]">部屋紹介
+                </label>
+                <label>
+                    <input type="checkbox" id="game" name="select[]">テレビ番組
+                </label>
+                <label>
+                    <input type="checkbox" id="game" name="select[]">YouTuber
+                </label>
+                <label>
+                    <input type="checkbox" id="game" name="select[]">凄技
+                </label>
+                <label>
+                    <input type="checkbox" id="game" name="select[]">音楽
+                </label>
+                <label>
+                    <input type="checkbox" id="game" name="select[]">筋肉
+                </label>
+            </form>
         </div>
     </div>
 </body>
