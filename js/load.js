c362b2dd85be2b4195f788475605db123998eb9b
GakuA <akazawagaku@gmail.com>
2017-11-30 19:09:14 +0900

    up

--- js/load.js
+++ js/load.js
@@ -17,7 +17,7 @@ $(function(){
         var tabText = "<div class='active'>トレンド</div>";
         $.each(arrSelect,
             function(key, value) {
-                tabText += "<div class='" + arrGenre[value] + "'>" + value + "</div>";
+                tabText += "<div class='" + value + "'>" + arrGenre[value] + "</div>";
             }
         );
         $(".tab").html(tabText);
