f24334b34301648fe80574ba6eec20fad145d6ce
GakuA <akazawagaku@gmail.com>
2017-11-30 18:42:46 +0900

    up

--- js/load.js
+++ js/load.js
@@ -1,5 +1,19 @@
+var arrGenre = {sports: "スポーツ",
+                technology: "テクノロジー",
+                news: "ニュース",
+                game: "ゲーム",
+                animal: "動物",
+                product: "商品紹介",
+                room: "部屋紹介",
+                tv: "テレビ番組",
+                youtuber: "YouTuber",
+                great: "凄技",
+                music: "音楽",
+                muscle: "筋肉"}
+
 $(function(){
     if ($.cookie("genre")) {
+        var arrSelect = $.cookie("genre").split(",");
         $(".main").show();
     } else {
         $(".first").show();
