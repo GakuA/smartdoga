7757ecffc83147f1feed3719dc39b2aa8ce01617
GakuA <akazawagaku@gmail.com>
2017-11-30 15:51:49 +0900

    up

--- index.php
+++ index.php
@@ -4,11 +4,11 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="css/style.css" type="text/css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="js/jquery.cookie.js"></script>
     <script src="js/load.js"></script>
     <script src="js/function.js"></script>
+    <link rel="stylesheet" href="css/style.css" type="text/css">
     <title>SmartTube</title>
 </head>
 <body>
