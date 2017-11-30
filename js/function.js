f3ef31964d3f1146157d3cc69ae28d21a553d5fd
GakuA <akazawagaku@gmail.com>
2017-11-30 15:42:04 +0900

    up

--- js/function.js
+++ js/function.js
@@ -4,51 +4,51 @@ $(function(){
     $("#firstBtn").click(function() {
         if ($("#sports").prop('checked')) {
             selectGenre.push("sports");
-            $(".sports").addClass("active");
+            $(".sports").addClass("on");
         }
         if ($("#technology").prop('checked')) {
             selectGenre.push("technology");
-            $(".technology").addClass("active");
+            $(".technology").addClass("on");
         }
         if ($("#news").prop('checked')) {
             selectGenre.push("news");
-            $(".news").addClass("active");
+            $(".news").addClass("on");
         }
         if ($("#game").prop('checked')) {
             selectGenre.push("game");
-            $(".game").addClass("active");
+            $(".game").addClass("on");
         }
         if ($("#animal").prop('checked')) {
             selectGenre.push("animal");
-            $(".animal").addClass("active");
+            $(".animal").addClass("on");
         }
         if ($("#product").prop('checked')) {
             selectGenre.push("product");
-            $(".product").addClass("active");
+            $(".product").addClass("on");
         }
         if ($("#room").prop('checked')) {
             selectGenre.push("room");
-            $(".room").addClass("active");
+            $(".room").addClass("on");
         }
         if ($("#tv").prop('checked')) {
             selectGenre.push("tv");
-            $(".tv").addClass("active");
+            $(".tv").addClass("on");
         }
         if ($("#youtuber").prop('checked')) {
             selectGenre.push("youtuber");
-            $(".youtuber").addClass("active");
+            $(".youtuber").addClass("on");
         }
         if ($("#great").prop('checked')) {
             selectGenre.push("great");
-            $(".great").addClass("active");
+            $(".great").addClass("on");
         }
         if ($("#music").prop('checked')) {
             selectGenre.push("music");
-            $(".music").addClass("active");
+            $(".music").addClass("on");
         }
         if ($("#muscle").prop('checked')) {
             selectGenre.push("muscle");
-            $(".muscle").addClass("active");
+            $(".muscle").addClass("on");
         }
 
         $.cookie("genre", selectGenre, 365 * 5);
