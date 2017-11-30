ee0a181f8cd5b7824ec968309f7f9013cb66025a
GakuA <akazawagaku@gmail.com>
2017-11-30 11:09:02 +0900

    up

--- js/function.js
+++ js/function.js
@@ -1,8 +1,41 @@
 $(function(){
     $("#firstBtn").click(function() {
         var selectGenre = [];
-        if ($("#sports:checked").val()) {
+        if ($("#sports").prop('checked')) {
             selectGenre.push("sports");
+        }
+        if ($("#technology").prop('checked')) {
+            selectGenre.push("technology");
+        }
+        if ($("#news").prop('checked')) {
+            selectGenre.push("news");
+        }
+        if ($("#game").prop('checked')) {
+            selectGenre.push("game");
+        }
+        if ($("#animal").prop('checked')) {
+            selectGenre.push("animal");
+        }
+        if ($("#product").prop('checked')) {
+            selectGenre.push("product");
+        }
+        if ($("#room").prop('checked')) {
+            selectGenre.push("room");
+        }
+        if ($("#tv").prop('checked')) {
+            selectGenre.push("tv");
+        }
+        if ($("#youtuber").prop('checked')) {
+            selectGenre.push("youtuber");
+        }
+        if ($("#great").prop('checked')) {
+            selectGenre.push("great");
+        }
+        if ($("#music").prop('checked')) {
+            selectGenre.push("music");
+        }
+        if ($("#muscle").prop('checked')) {
+            selectGenre.push("muscle");
         }
     });
 });
