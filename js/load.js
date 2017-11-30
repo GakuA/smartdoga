f9fe87ac3737b35c7d1fc1ba252a1feaa1cf910d
GakuA <akazawagaku@gmail.com>
2017-11-30 20:01:37 +0900

    up

--- js/load.js
+++ js/load.js
@@ -1,17 +1,17 @@
 var arrGenre = {
-                sports: "スポーツ",
-                technology: "テクノロジー",
-                news: "ニュース",
-                game: "ゲーム",
-                animal: "動物",
-                product: "商品紹介",
-                room: "部屋紹介",
-                tv: "テレビ番組",
-                youtuber: "YouTuber",
-                great: "凄技",
-                music: "音楽",
-                muscle: "筋肉"
-                };
+    sports: "スポーツ",
+    technology: "テクノロジー",
+    news: "ニュース",
+    game: "ゲーム",
+    animal: "動物",
+    product: "商品紹介",
+    room: "部屋紹介",
+    tv: "テレビ番組",
+    youtuber: "YouTuber",
+    great: "凄技",
+    music: "音楽",
+    muscle: "筋肉"
+};
 
 $(function(){
     if ($.cookie("genre")) {
@@ -22,9 +22,9 @@ $(function(){
                 tabText += "<div class='" + value + "'>" + arrGenre[value] + "</div>";
             }
         );
-        //$(document).on("html", ".tab", function() {
-        //    $(this).html(tabText);
-        //});
+        $(document).on("html", ".tab", function() {
+            $(this).html(tabText);
+        });
         $(".main").show();
     } else {
         $(".first").show();
