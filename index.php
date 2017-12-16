53043cad5185196443b9e091db5d6e48df2b81fe
GakuA <akazawagaku@gmail.com>
2017-12-16 13:33:42 +0900

    up

--- index.php
+++ index.php
@@ -29,53 +29,51 @@
         </div>
 
         <div class="content">
-                <div class="first">
-                    <div>
-                        <h4>興味があるジャンルを選択して下さい</h4>
-                    </div>
-                    <form class="select">
-                        <label>
-                            <input type="checkbox" id="sports">スポーツ
-                        </label>
-                        <label>
-                            <input type="checkbox" id="technology">テクノロジー
-                        </label>
-                        <label>
-                            <input type="checkbox" id="news">ニュース
-                        </label>
-                        <label>
-                            <input type="checkbox" id="game">ゲーム
-                        </label>
-                        <label>
-                            <input type="checkbox" id="animal">動物
-                        </label>
-                        <label>
-                            <input type="checkbox" id="product">商品紹介
-                        </label>
-                        <label>
-                            <input type="checkbox" id="room">部屋紹介
-                        </label>
-                        <label>
-                            <input type="checkbox" id="tv">テレビ番組
-                        </label>
-                        <label>
-                            <input type="checkbox" id="youtuber">YouTuber
-                        </label>
-                        <label>
-                            <input type="checkbox" id="great">凄技
-                        </label>
-                        <label>
-                            <input type="checkbox" id="music">音楽
-                        </label>
-                        <label>
-                            <input type="checkbox" id="muscle">筋肉
-                        </label>
-                        <input type="button" id="firstBtn" value="はじめる">
-                    </form>
+            <div class="setting">
+                <div>
+                    <h4>興味があるジャンルを選択して下さい</h4>
                 </div>
-
+                <form class="select">
+                    <label>
+                        <input type="checkbox" id="sports">スポーツ
+                    </label>
+                    <label>
+                        <input type="checkbox" id="technology" name="select[]">テクノロジー
+                    </label>
+                    <label>
+                        <input type="checkbox" id="news" name="select[]">ニュース
+                    </label>
+                    <label>
+                        <input type="checkbox" id="game" name="select[]">ゲーム
+                    </label>
+                    <label>
+                        <input type="checkbox" id="animal" name="select[]">動物
+                    </label>
+                    <label>
+                        <input type="checkbox" id="product" name="select[]">商品紹介
+                    </label>
+                    <label>
+                        <input type="checkbox" id="room" name="select[]">部屋紹介
+                    </label>
+                    <label>
+                        <input type="checkbox" id="tv" name="select[]">テレビ番組
+                    </label>
+                    <label>
+                        <input type="checkbox" id="youtuber" name="select[]">YouTuber
+                    </label>
+                    <label>
+                        <input type="checkbox" id="great" name="select[]">凄技
+                    </label>
+                    <label>
+                        <input type="checkbox" id="music" name="select[]">音楽
+                    </label>
+                    <label>
+                        <input type="checkbox" id="muscle" name="select[]">筋肉
+                    </label>
+                    <input type="button" id="settingBtn" value="決定">
+                </form>
+            </div>
         </div>
-
     </div>
 </body>
 </html>
