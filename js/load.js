9f6a696ef856aedc08d8b81bee75a42c0c145276
GakuA <akazawagaku@gmail.com>
2017-12-31 21:15:10 +0900

    up

--- js/load.js
+++ js/load.js
@@ -14,7 +14,7 @@ var arrGenre = {
 };
 
 $(function(){
-    $('.content > .setting').sortable();
+    $('.content .deleteContents').sortable();
     if ($.cookie("genre")) {
         var arrSelect = $.cookie("genre").split(",");
         var tabText = "<div class='trends active'>トレンド</div>";
